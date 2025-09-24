580027d3e296c68cbf422140524af68576321065
Dwi Cahya Novita <dwicahyanovitaa@gmail.com>
2025-09-24 23:55:59 +0700

    First Commit

---
+++ biodata.py
@@ -0,0 +1,9 @@
+# Definisi variabel
+nama = "Dwi Cahya Novita"
+umur = 21
+hobi = "membaca"
+
+# Print 
+print(f"Nama saya adalah {nama}.")
+print(f"Saat ini saya berumur {umur} tahun.")
+print(f"Hobi saya adalah {hobi}.")
