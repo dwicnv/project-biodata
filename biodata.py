1cebee89f60cb566c82a2c9057a27df544e38191
Dwi Cahya Novita <dwicahyanovitaa@gmail.com>
2025-09-25 00:44:42 +0700

    menambahkan variabel asal kota

--- biodata.py
+++ biodata.py
@@ -2,8 +2,10 @@
 nama = "Dwi Cahya Novita"
 umur = 21
 hobi = "membaca"
+asal = "Yogyakarta"
 
 # Print 
 print(f"Nama saya adalah {nama}.")
 print(f"Saat ini saya berumur {umur} tahun.")
 print(f"Hobi saya adalah {hobi}.")
+print(f"Saya berasal dari kota {asal}.")
